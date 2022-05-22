d61d772be03ff18f61556fd3b7096deb114399f6
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-05-22 12:25:23 +0530

    #include<stdio.h> #include<math.h> int nod(int num) {     int c=0;     while(num>0)     {         c++;         num/=10;
        }
        return c;
    }
    int main()
    {
        long long int a;
        int b,ft,lt,diff;
        scanf("%ld%d",&a,&b);
        int x=pow(10,b);
        ft=a%x;
        while(nod(a)>b)
        {
            a/=10;
        }
        lt=a;
        diff=ft-lt;
        if(diff<0)
        {
            diff*=-1;
        }
        printf("%d",diff);
    }

---
+++ Absolute_difference_numbers.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+int nod(int num)
+{
+    int c=0;
+    while(num>0)
+    {
+        c++;
+        num/=10;
+        
+    }
+    return c;
+}
+int main()
+{
+    long long int a;
+    int b,ft,lt,diff;
+    scanf("%ld%d",&a,&b);
+    int x=pow(10,b);
+    ft=a%x;
+    while(nod(a)>b)
+    {
+        a/=10;
+    }
+    lt=a;
+    diff=ft-lt;
+    if(diff<0)
+    {
+        diff*=-1;
+    }
+    printf("%d",diff);
+}
