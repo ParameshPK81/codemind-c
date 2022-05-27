7fd12497dd6ff6851ee3395a79222e7e35620379
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-05-28 00:18:53 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {        for(j=1;j<=i;j++)        {            printf("%d",j);        }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,17 +5,10 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
-        {
-            if(i==j)
-            {
-                printf("0");
-            }
-            else
-            {
-                printf("x");
-            }
-        }
+       for(j=1;j<=i;j++)
+       {
+           printf("%d",j);
+       }
         printf("
 ");
     }
