2eab85d5994d7b6ec3d54d75608521aeb68b247b
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-05-28 01:24:03 +0530

    #include<stdio.h> int main() {     int n,i,j,r,c,a[100][100],s=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);                 s+=a[i][j];         }     }     printf("%d",s);
    }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,r,c,a[100][100],s=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+                s+=a[i][j];
+        }
+    }
+    printf("%d",s);
+    
+    
+}
