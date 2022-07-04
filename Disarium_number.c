9b1895e8e35d6941474bd2cb842f22681736fed0
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-07-04 19:10:04 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,temp,d,x=0,res=0;     scanf("%d",&n);     temp=n;     while(n)     {         d=n%10;         x=x+1;         n=n/10;     }     n=temp;     while(n)     {         d=n%10;         res=res+pow(d,x);         n=n/10;         x--;     }     if(temp==res)     {         printf("True");     }     else     {         printf("False");     } }

--- Disarium_number.c
+++ Disarium_number.c
@@ -2,23 +2,24 @@
 #include<math.h>
 int main()
 {
-   int n;
-   scanf("%d",&n);
-   int rev=0,b,i,sq,sum=0,temp=n;
-   while(temp>0)
-   {
-       b=temp%10;
-       rev=(rev*10)+b;
-       temp/=10;
-   }
-   for(i=1;rev>0;i++)
-   {
-       b=rev%10;
-       sq=pow(b,i);
-       sum+=sq;
-       rev=rev/10;
-   }
-    if(sum==n)
+    int n,temp,d,x=0,res=0;
+    scanf("%d",&n);
+    temp=n;
+    while(n)
+    {
+        d=n%10;
+        x=x+1;
+        n=n/10;
+    }
+    n=temp;
+    while(n)
+    {
+        d=n%10;
+        res=res+pow(d,x);
+        n=n/10;
+        x--;
+    }
+    if(temp==res)
     {
         printf("True");
     }
