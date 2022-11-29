b1b7993578a5609b19f02a7406ed6de9f899951a
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-11-29 15:30:34 +0530

    #include<stdio.h> int main(){     int n,k;     scanf("%d",&n);     k=n;     for(int i=1;i<=n;i++){         for(int j=1;j<=n;j++){             if( i==j || j==(n-i+1) ){                 printf("%d ",k);             }             else{             printf(" ");             }         }         k-=1;         printf(" ");     } }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,j;
+int main(){
+    int n,k;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)//for(j=1;j<=i;j++)
-    {
-       for(j=i;j<=n;j++)//for(i=n;i<=n;i++)
-       {
-           printf("%d ",j);
-       }
+    k=n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if( i==j || j==(n-i+1) ){
+                printf("%d ",k);
+            }
+            else{
+            printf(" ");
+            }
+        }
+        k-=1;
         printf("
 ");
     }
-    
 }
